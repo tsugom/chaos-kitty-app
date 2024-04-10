5011b87c9e531fd931a69faec94c5e2339bc123d
tsugom <90293000+tsugom@users.noreply.github.com>
2024-04-10 21:43:22 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -68,4 +68,4 @@ def delete(todo_id):
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(debug=False,host='0.0.0.0', port=8080)
+    app.run(debug=False,host='0.0.0.0', port=8080, threaded=True)
